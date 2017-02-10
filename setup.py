b6a0fbdae2d763c2c35fc4c45652f5d3f4dfb97e
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2017-02-10 15:07:55 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="openwhisk_docker_action",
+    version="0.1.0",
+    description="A class to make writing openwhisk docker actions easier to write in python",
+    license="MIT",
+    author="Joshua B. Smith",
+    packages=find_packages(),
+    install_requires=[ 'flask' ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
