236bec35ea8763ff0d9b762b88ba228dd1388b7a
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2017-02-16 13:16:17 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="openwhisk_docker_action",
-    version="0.1.3",
+    version="0.1.4",
     description="A class to make writing openwhisk docker actions easier to write in python",
     license="MIT",
     author="Joshua B. Smith",
